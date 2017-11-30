ba2a2970f4d9b514a77e85297d0e02febdbf5483
Jason Brown <jasedbrown@gmail.com>
2017-11-30 14:01:46 -0800

    refactored CommitLogStressTest to execute each commitlog mode as a separate subclass, in order to reduce overall test time

--- test/long/org/apache/cassandra/db/commitlog/CommitLogStressTest.java
+++ test/long/org/apache/cassandra/db/commitlog/CommitLogStressTest.java
@@ -33,6 +33,7 @@ import com.google.common.util.concurrent.RateLimiter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -58,8 +59,8 @@ import org.apache.cassandra.security.EncryptionContext;
 import org.apache.cassandra.security.EncryptionContextGenerator;
 
 
-@RunWith(Parameterized.class)
-public class CommitLogStressTest
+@Ignore
+public abstract class CommitLogStressTest
 {
     static
     {
@@ -87,53 +88,54 @@ public class CommitLogStressTest
         return hash;
     }
 
-    public static void main(String[] args) throws Exception
-    {
-        try
-        {
-            if (args.length >= 1)
-            {
-                NUM_THREADS = Integer.parseInt(args[0]);
-                System.out.println("Setting num threads to: " + NUM_THREADS);
-            }
-
-            if (args.length >= 2)
-            {
-                numCells = Integer.parseInt(args[1]);
-                System.out.println("Setting num cells to: " + numCells);
-            }
-
-            if (args.length >= 3)
-            {
-                cellSize = Integer.parseInt(args[1]);
-                System.out.println("Setting cell size to: " + cellSize + " be aware the source corpus may be small");
-            }
-
-            if (args.length >= 4)
-            {
-                rateLimit = Integer.parseInt(args[1]);
-                System.out.println("Setting per thread rate limit to: " + rateLimit);
-            }
-            initialize();
-
-            for (Object[] params : buildParameterizedVariants())
-            {
-                ParameterizedClass commitLogCompression = (ParameterizedClass) params[0];
-                EncryptionContext encryptionContext = (EncryptionContext) params[1];
-                CommitLogStressTest tester = new CommitLogStressTest(commitLogCompression, encryptionContext);
-                tester.cleanDir();
-                tester.testFixedSize();
-            }
-        }
-        catch (Throwable e)
-        {
-            e.printStackTrace(System.err);
-        }
-        finally
-        {
-            System.exit(0);
-        }
-    }
+    // TODO:JEB resolve this
+//    public static void main(String[] args) throws Exception
+//    {
+//        try
+//        {
+//            if (args.length >= 1)
+//            {
+//                NUM_THREADS = Integer.parseInt(args[0]);
+//                System.out.println("Setting num threads to: " + NUM_THREADS);
+//            }
+//
+//            if (args.length >= 2)
+//            {
+//                numCells = Integer.parseInt(args[1]);
+//                System.out.println("Setting num cells to: " + numCells);
+//            }
+//
+//            if (args.length >= 3)
+//            {
+//                cellSize = Integer.parseInt(args[1]);
+//                System.out.println("Setting cell size to: " + cellSize + " be aware the source corpus may be small");
+//            }
+//
+//            if (args.length >= 4)
+//            {
+//                rateLimit = Integer.parseInt(args[1]);
+//                System.out.println("Setting per thread rate limit to: " + rateLimit);
+//            }
+//            initialize();
+//
+//            for (Object[] params : buildParameterizedVariants())
+//            {
+//                ParameterizedClass commitLogCompression = (ParameterizedClass) params[0];
+//                EncryptionContext encryptionContext = (EncryptionContext) params[1];
+//                CommitLogStressTest tester = new CommitLogStressTest(commitLogCompression, encryptionContext);
+//                tester.cleanDir();
+//                tester.testFixedSize();
+//            }
+//        }
+//        catch (Throwable e)
+//        {
+//            e.printStackTrace(System.err);
+//        }
+//        finally
+//        {
+//            System.exit(0);
+//        }
+//    }
 
     private boolean failed = false;
     private volatile boolean stop = false;
@@ -145,10 +147,6 @@ public class CommitLogStressTest
     {
         DatabaseDescriptor.setCommitLogCompression(commitLogCompression);
         DatabaseDescriptor.setEncryptionContext(encryptionContext);
-
-        DatabaseDescriptor.setCommitLogSyncBatchWindow(1);
-        DatabaseDescriptor.setCommitLogSyncGroupWindow(1);
-        DatabaseDescriptor.setCommitLogSyncPeriod(30);
         DatabaseDescriptor.setCommitLogSegmentSize(32);
     }
 
@@ -230,13 +228,9 @@ public class CommitLogStressTest
         try
         {
             DatabaseDescriptor.setCommitLogLocation(location);
-            for (CommitLogSync sync : CommitLogSync.values())
-            {
-                DatabaseDescriptor.setCommitLogSync(sync);
-                CommitLog commitLog = new CommitLog(CommitLogArchiver.disabled()).start();
-                testLog(commitLog);
-                assert !failed;
-            }
+            CommitLog commitLog = new CommitLog(CommitLogArchiver.disabled()).start();
+            testLog(commitLog);
+            assert !failed;
         }
         finally
         {
